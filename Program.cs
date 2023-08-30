c787f2a2c2173789cf925c4e8aea76c0f4fe0a79
Chieu Phan <mrblvck23@gmail.com>
2023-08-30 16:08:39 +0700

    buoi 3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMinh_Buoi3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a
+            //TamGiac tamgiac = new TamGiac();
+            TamGiac tamgiac1 = new TamGiac(3,4,5);
+            Console.WriteLine("Chu vi tam giac: {0}", tamgiac1.TinhChuVi());
+            Console.WriteLine("Dien tich tam giac: {0:N2}", tamgiac1.TinhDienTich());
+            tamgiac1.OutPut();
+            //b
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TamGiac.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMinh_Buoi3
+{
+    class TamGiac
+    {
+        private double ma;
+
+        public double Ma
+        {
+            get { return ma; }
+            set { ma = value; }
+        }
+        private double mb;
+
+        public double Mb
+        {
+            get { return mb; }
+            set { mb = value; }
+        }
+        private double mc;
+
+        public double Mc
+        {
+            get { return mc; }
+            set { mc = value; }
+        }
+
+        public TamGiac()
+        {
+            ma = 0.0;
+            mb = 0.0;
+            mc = 0.0;
+        }
+        public TamGiac(double ma, double mb, double mc)
+        {
+            //
+            if (ma == 0 || mb == 0 || mc == 0 || (ma + mb) < mc || (mb + mc) < ma || (ma + mc) < mb)
+            //
+            {
+                this.ma = 0;
+                this.mb = 0;
+                this.mc = 0;
+            }
+            else
+            {
+                this.ma = ma;
+                this.mb = mb;
+                this.mc = mc;
+            }
+        }
+
+
+        public double TinhChuVi()
+        {
+            return ma + mb + mc;
+        }
+        public double TinhDienTich()
+        {
+            return (1.0 / 4) * (Math.Sqrt((ma + mb + mc) * (ma + mb - mc) * (mb + mc - ma) * (mc + ma - mb)));
+        }
+        public string KieuTamGiac()
+        {
+            if ((ma + mb) >= mc || (mb + mc) >= ma || (ma + mc) >= mb)
+            {
+                if ((ma == mb && ma != mc) || (mb == mc && mb != ma) || (ma == mc && ma != mb))
+                    return "Tam Giac Can";
+                else if (ma == mb && mb == mc)
+                    return "Tam Giac Deu";
+                else if (Math.Pow(ma,2) + Math.Pow(mb,2) == Math.Pow(mc,2) || Math.Pow(mb,2) + Math.Pow(mc,2) == Math.Pow(ma,2) || Math.Pow(ma,2) + Math.Pow(mc,2) == Math.Pow(mb,2))
+                    return "Tam Giac Vuong";
+                else
+                    return "Tam Giac Thuong";
+            }
+            else
+                return "Khong Phai Tam Giac";
+        }
+        public void OutPut()
+        {
+            Console.WriteLine("Thong tin 3 canh: ");
+            Console.WriteLine("a={0}\nb={1}\nc={2}", ma, mb, mc);
+            Console.WriteLine("Kieu tam giac: "+KieuTamGiac());
+            Console.WriteLine("Chu vi: {0}\nDien tich: {1}", TinhChuVi(), TinhDienTich());
+        }
+    }
+}
